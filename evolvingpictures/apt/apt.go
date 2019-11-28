fe6a18b3f0201e82996d653066fe577f81abd941
Nikolay <CoolDualist@mail.ru>
2019-11-28 23:57:38 +0300

    add random texture

--- evolvingpictures/apt/apt.go
+++ evolvingpictures/apt/apt.go
@@ -1,27 +1,95 @@
 package apt
 
 import (
+	"experiments/experiments/noise"
 	"fmt"
 	"math"
+	"math/rand"
+	"strconv"
 )
 
 type Node interface {
 	Eval(x, y float32) float32
 	String() string
+	AddRandom(node Node)
+	NodeCounts() (nodeCount, nilCount int)
 }
 
-type LeafNode struct {}
+type LeafNode struct{}
+
+func (leaf *LeafNode) AddRandom(node Node) {
+	//panic(`ERROR: You tried to add a node to a leaf node`)
+	fmt.Println(` add a node to a leaf node`)
+}
+
+func (leaf *LeafNode) NodeCounts() (nodeCount, nilCount int) {
+	return 1, 0
+}
 
 type SingleNode struct {
 	Child Node
 }
 
+func (single *SingleNode) AddRandom(node Node) {
+	if single.Child == nil {
+		single.Child = node
+	} else {
+		single.Child.AddRandom(node)
+	}
+}
+
+func (single *SingleNode) NodeCounts() (nodeCount, nilCount int) {
+	if single.Child == nil {
+		return 1, 1
+	} else {
+		childNodeCount, childNilCount := single.Child.NodeCounts()
+		return 1 + childNodeCount, childNilCount
+	}
+}
+
 type DoubleNode struct {
-	LeftChild Node
+	LeftChild  Node
 	RightChild Node
 }
 
-type OpSin SingleNode
+func (double *DoubleNode) AddRandom(node Node) {
+	if rand.Intn(2) == 0 {
+		if double.LeftChild == nil {
+			double.LeftChild = node
+		} else {
+			double.LeftChild.AddRandom(node)
+		}
+	} else {
+		if double.RightChild == nil {
+			double.RightChild = node
+		} else {
+			double.RightChild.AddRandom(node)
+		}
+	}
+}
+
+func (double *DoubleNode) NodeCounts() (nodeCount, nilCount int) {
+	var leftCount, leftNilCount, rightCount, rightNilCount int
+	if double.LeftChild == nil {
+		leftNilCount = 1
+		leftCount = 0
+	} else {
+		leftCount, leftNilCount = double.LeftChild.NodeCounts()
+	}
+
+	if double.RightChild == nil {
+		rightNilCount = 1
+		rightCount = 0
+	} else {
+		rightCount, rightNilCount = double.RightChild.NodeCounts()
+	}
+	return 1 + leftCount + rightCount, leftNilCount + rightNilCount
+}
+
+type OpSin struct {
+	SingleNode
+}
+
 func (op *OpSin) Eval(x, y float32) float32 {
 	return float32(math.Sin(float64(op.Child.Eval(x, y))))
 }
@@ -30,23 +98,70 @@ func (op *OpSin) String() string {
 	return fmt.Sprintf(`( Sin %s)`, op.Child.String())
 }
 
-type OpPlus DoubleNode
+type OpCos struct {
+	SingleNode
+}
+
+func (op *OpCos) Eval(x, y float32) float32 {
+	return float32(math.Cos(float64(op.Child.Eval(x, y))))
+}
+
+func (op *OpCos) String() string {
+	return fmt.Sprintf(`( Cos %s)`, op.Child.String())
+}
+
+type OpAtan struct {
+	SingleNode
+}
+
+func (op *OpAtan) Eval(x, y float32) float32 {
+	return float32(math.Atan(float64(op.Child.Eval(x, y))))
+}
+
+func (op *OpAtan) String() string {
+	return fmt.Sprintf(`( Atan %s)`, op.Child.String())
+}
+
+type OpNoise struct {
+	DoubleNode
+}
+
+func (op *OpNoise) Eval(x, y float32) float32 {
+	return 80*noise.Snoise2(op.LeftChild.Eval(x, y), op.RightChild.Eval(x, y)) - 2.0
+}
+
+func (op *OpNoise) String() string {
+	return fmt.Sprintf(`( SimplexNoise %s %s )`, op.LeftChild.String(), op.RightChild.String())
+}
+
+type OpPlus struct {
+	DoubleNode
+}
+
 func (op *OpPlus) Eval(x, y float32) float32 {
 	return op.LeftChild.Eval(x, y) + op.RightChild.Eval(x, y)
 }
+
 func (op *OpPlus) String() string {
 	return fmt.Sprintf(`( + %s %s )`, op.LeftChild.String(), op.RightChild.String())
 }
 
-type OpMinus DoubleNode
+type OpMinus struct {
+	DoubleNode
+}
+
 func (op *OpMinus) Eval(x, y float32) float32 {
 	return op.LeftChild.Eval(x, y) - op.RightChild.Eval(x, y)
 }
+
 func (op *OpMinus) String() string {
 	return fmt.Sprintf(`( - %s %s )`, op.LeftChild.String(), op.RightChild.String())
 }
 
-type OpMult DoubleNode
+type OpMult struct {
+	DoubleNode
+}
+
 func (op *OpMult) Eval(x, y float32) float32 {
 	return op.LeftChild.Eval(x, y) * op.RightChild.Eval(x, y)
 }
@@ -54,7 +169,10 @@ func (op *OpMult) String() string {
 	return fmt.Sprintf(`( * %s %s )`, op.LeftChild.String(), op.RightChild.String())
 }
 
-type OpDiv DoubleNode
+type OpDiv struct {
+	DoubleNode
+}
+
 func (op *OpDiv) Eval(x, y float32) float32 {
 	return op.LeftChild.Eval(x, y) / op.RightChild.Eval(x, y)
 }
@@ -62,7 +180,10 @@ func (op *OpDiv) String() string {
 	return fmt.Sprintf(`( / %s %s )`, op.LeftChild.String(), op.RightChild.String())
 }
 
-type OpAtan2 DoubleNode
+type OpAtan2 struct {
+	DoubleNode
+}
+
 func (op *OpAtan2) Eval(x, y float32) float32 {
 	return float32(math.Atan2(float64(y), float64(x)))
 }
@@ -70,7 +191,10 @@ func (op *OpAtan2) String() string {
 	return fmt.Sprintf(`( OpAtan2 %s %s )`, op.LeftChild.String(), op.RightChild.String())
 }
 
-type OpX  LeafNode
+type OpX struct {
+	LeafNode
+}
+
 func (op *OpX) Eval(x, y float32) float32 {
 	return x
 }
@@ -79,11 +203,66 @@ func (op *OpX) String() string {
 	return "X"
 }
 
-type OpY LeafNode
+type OpY struct {
+	LeafNode
+}
+
 func (op *OpY) Eval(x, y float32) float32 {
 	return y
 }
 
 func (op *OpY) String() string {
 	return "Y"
+}
+
+type OpConstant struct {
+	LeafNode
+	value float32
+}
+
+func (op *OpConstant) Eval(x, y float32) float32 {
+	return op.value
+}
+
+func (op *OpConstant) String() string {
+	return strconv.FormatFloat(float64(op.value), 'f', 3, 32)
+}
+
+func GetRandomNode() Node {
+	switch rand.Intn(9) {
+	case 0:
+		return &OpPlus{}
+	case 1:
+		return &OpMinus{}
+	case 2:
+		return &OpMult{}
+	case 3:
+		return &OpDiv{}
+	case 4:
+		return &OpAtan2{}
+	case 5:
+		return &OpAtan{}
+	case 6:
+		return &OpCos{}
+	case 7:
+		return &OpSin{}
+	case 8:
+		return &OpNoise{}
+	}
+	panic(`ERROR: Get Random Noise Failed`)
+}
+
+func GetRandomLeaf() Node {
+	switch rand.Intn(3) {
+	case 0:
+		return &OpX{}
+	case 1:
+		return &OpY{}
+	case 2:
+		return &OpConstant{
+			LeafNode: LeafNode{},
+			value:    rand.Float32()*2 - 1,
+		}
+	}
+	panic(`ERROR: Get Leaf`)
 }
--- evolvingpictures/evolvingpictures.go
+++ evolvingpictures/evolvingpictures.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-	"experiments/evolvingpictures/apt"
+	"experiments/experiments/evolvingpictures/apt"
 	"github.com/veandco/go-sdl2/sdl"
+	"math/rand"
 	"time"
 )
 
 const (
-	windowWidth = 1200
+	windowWidth  = 1200
 	windowHeight = 800
-	windowDepth = 100
+	windowDepth  = 100
 )
 
 type audioState struct {
 	explosionBytes []byte
-	deviceId sdl.AudioDeviceID
-	audiSpec *sdl.AudioSpec
+	deviceId       sdl.AudioDeviceID
+	audiSpec       *sdl.AudioSpec
 }
 
 type mouseState struct {
-	leftButton bool
+	leftButton  bool
 	rightButton bool
-	x, y int32
+	x, y        int32
 }
 
 func getMouseState() mouseState {
@@ -38,20 +39,19 @@ type rgba struct {
 	r, g, b byte
 }
 
-
-func clear(pixels []byte)  {
+func clear(pixels []byte) {
 	for i := range pixels {
 		pixels[i] = 0
 	}
 }
 
 func setPixel(x, y int, c rgba, pixels []byte) {
-	index := (y* windowWidth + x ) * 4
+	index := (y*windowWidth + x) * 4
 
 	if index <= len(pixels)-4 && index >= 0 {
 		pixels[index] = c.r
-		pixels[index + 1] = c.g
-		pixels[index + 2] = c.b
+		pixels[index+1] = c.g
+		pixels[index+2] = c.b
 	}
 }
 
@@ -59,34 +59,34 @@ func pixelsToTexture(renderer *sdl.Renderer, pixels []byte, w, h int) *sdl.Textu
 	if tex, err := renderer.CreateTexture(sdl.PIXELFORMAT_ABGR8888, sdl.TEXTUREACCESS_STREAMING, int32(w), int32(h)); err != nil {
 		panic(err)
 	} else {
-		if err := tex.Update(nil, pixels, w * 4); err != nil {
+		if err := tex.Update(nil, pixels, w*4); err != nil {
 			panic(err)
 		}
 		return tex
 	}
 }
 
-func aptToTexture(redNode, greedNode, blueNode apt.Node,  w, h int, renderer *sdl.Renderer) *sdl.Texture {
+func aptToTexture(redNode, greedNode, blueNode apt.Node, w, h int, renderer *sdl.Renderer) *sdl.Texture {
 	var (
-		scale float32 = 255 / 2
-		offset = -1.0 * scale
-		pixels = make([]byte, w * h * 4)
-		pisxelsIndex = 0
+		scale        float32 = 255 / 2
+		offset               = -1.0 * scale
+		pixels               = make([]byte, w*h*4)
+		pisxelsIndex         = 0
 	)
-	for yi := 0; yi < h ; yi++ {
-		y := float32(yi) / float32(h) * 2 - 1
-		for xi := 0; xi < w ; xi++ {
+	for yi := 0; yi < h; yi++ {
+		y := float32(yi)/float32(h)*2 - 1
+		for xi := 0; xi < w; xi++ {
 			var (
-				x = float32(xi) / float32(w) * 2 - 1
+				x = float32(xi)/float32(w)*2 - 1
 				r = redNode.Eval(x, y)
 				g = greedNode.Eval(x, y)
 				b = blueNode.Eval(x, y)
 			)
-			pixels[pisxelsIndex] = byte(r * scale - offset)
+			pixels[pisxelsIndex] = byte(r*scale - offset)
 			pisxelsIndex++
-			pixels[pisxelsIndex] = byte(g * scale - offset)
+			pixels[pisxelsIndex] = byte(g*scale - offset)
 			pisxelsIndex++
-			pixels[pisxelsIndex] = byte(b * scale - offset)
+			pixels[pisxelsIndex] = byte(b*scale - offset)
 			pisxelsIndex++
 			pisxelsIndex++ // skip alpha
 		}
@@ -94,7 +94,7 @@ func aptToTexture(redNode, greedNode, blueNode apt.Node,  w, h int, renderer *sd
 	return pixelsToTexture(renderer, pixels, w, h)
 }
 
-func main()  {
+func main() {
 	sdl.LogSetAllPriority(sdl.LOG_PRIORITY_VERBOSE)
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		panic(err)
@@ -106,7 +106,7 @@ func main()  {
 	if err != nil {
 		panic(err)
 	}
-	defer  windows.Destroy()
+	defer windows.Destroy()
 
 	renderer, err := sdl.CreateRenderer(windows, -1, sdl.RENDERER_ACCELERATED)
 	if err != nil {
@@ -126,27 +126,76 @@ func main()  {
 	//	audiSpec:       audioSpec,
 	//}
 	//defer sdl.FreeWAV(explosionBytes)
-    sdl.SetHint(sdl.HINT_RENDER_SCALE_QUALITY, "1")
+	rand.Seed(time.Now().UTC().UnixNano())
+	sdl.SetHint(sdl.HINT_RENDER_SCALE_QUALITY, "1")
 	var (
-		elapsedTime float32
+		elapsedTime       float32
 		currentMouseState = getMouseState()
-		//prevMouseState = getMouseState()
-		sine = &apt.OpSin{
-			Child: &apt.OpX{},
-		}
-		plus = &apt.OpPlus{
-			LeftChild:  sine,
-			RightChild: &apt.OpY{},
-		}
-		tex = APTToTexture(plus, sine, windowWidth, windowHeight, renderer)
+		aptR              = apt.GetRandomNode()
+		aptG              = apt.GetRandomNode()
+		aptB              = apt.GetRandomNode()
+
+	//prevMouseState = getMouseState()
+	//noise = &apt.OpNoise{
+	//	LeftChild:  &apt.OpX{},
+	//	RightChild: &apt.OpY{},
+	//}
+	//atan2 = &apt.OpMult{
+	//	LeftChild: &apt.OpX{},
+	//	RightChild: noise,
+	//}
+	//sine = &apt.OpSin{
+	//	Child: atan2,
+	//}
+	//plus = &apt.OpPlus{
+	//	LeftChild:  &apt.OpY{},
+	//	RightChild: sine,
+	//}
 	)
+	var num = rand.Intn(20)
+	for i := 0; i < num; i++ {
+		aptR.AddRandom(apt.GetRandomNode())
+	}
+	num = rand.Intn(20)
+	for i := 0; i < num; i++ {
+		aptG.AddRandom(apt.GetRandomNode())
+	}
+	num = rand.Intn(20)
+	for i := 0; i < num; i++ {
+		aptB.AddRandom(apt.GetRandomNode())
+	}
+
+	for {
+		_, nilCount := aptR.NodeCounts()
+		if nilCount == 0 {
+			break
+		}
+		aptR.AddRandom(apt.GetRandomLeaf())
+	}
+	for {
+		_, nilCount := aptG.NodeCounts()
+		if nilCount == 0 {
+			break
+		}
+		aptG.AddRandom(apt.GetRandomLeaf())
+	}
+	for {
+		_, nilCount := aptB.NodeCounts()
+		if nilCount == 0 {
+			break
+		}
+		aptB.AddRandom(apt.GetRandomLeaf())
+	}
+
+	tex := aptToTexture(aptR, aptG, aptB, windowWidth, windowHeight, renderer)
+
 	for {
 		frameStart := time.Now()
 
 		currentMouseState = getMouseState()
 
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch e:= event.(type) {
+			switch e := event.(type) {
 			case *sdl.QuitEvent:
 				return
 			case *sdl.TouchFingerEvent:
@@ -158,7 +207,7 @@ func main()  {
 			}
 		}
 
-		if err = renderer.Copy(tex, nil,nil); err != nil {
+		if err = renderer.Copy(tex, nil, nil); err != nil {
 			panic(err)
 		}
 
